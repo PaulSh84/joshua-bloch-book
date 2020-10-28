219adfd76704b8ffd57cdd12dfed7cb0b085038a
Paul Shirmovski <pshirmovski@gmail.com>
2020-10-28 23:51:39 +0100

    streams or iteration?

---
+++ src/main/java/lambdasandstreams/usestreamsjudiciously/StreamsMakeItVeryEasyTodoTheseThings.java
@@ -0,0 +1,16 @@
+package lambdasandstreams.usestreamsjudiciously;
+
+public class StreamsMakeItVeryEasyTodoTheseThings {
+
+    /**
+     * • Uniformly transform sequences of elements
+     *
+     * • Filter sequences of elements
+     *
+     * • Combine sequences of elements using a single operation (for example to add them, concatenate them, or compute their minimum)
+     *
+     * • Accumulate sequences of elements into a collection, perhaps grouping them by some common attribute
+     *
+     * • Search a sequence of elements for an element satisfying some criterion
+     */
+}
---
+++ src/main/java/lambdasandstreams/usestreamsjudiciously/ThingsYouCantDoInFunctionObject.java
@@ -0,0 +1,18 @@
+package lambdasandstreams.usestreamsjudiciously;
+
+public class ThingsYouCantDoInFunctionObject {
+
+    /**
+     * From a lambda, you can read only final or effectively final variables.
+     * You can modify any local variables.
+     */
+
+    /**
+     * From lambda you can't return from the enclosing method, break or continue an enclosing loop,
+     * or throw any checked exception that this method is declared to throw.
+     */
+
+    /**
+     *
+     */
+}
