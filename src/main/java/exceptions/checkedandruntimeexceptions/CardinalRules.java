1a003e6c623d67cea879d42f8b47999bd9224a4c
Paul Shirmovski <pshirmovski@gmail.com>
2020-12-01 23:09:08 +0100

    rules for checked exceptions and runtime exceptions

---
+++ src/main/java/exceptions/checkedandruntimeexceptions/CardinalRules.java
@@ -0,0 +1,12 @@
+package exceptions.checkedandruntimeexceptions;
+
+public class CardinalRules {
+
+    /* use checked exceptions for conditions from which the caller can
+        reasonably be expected to recover.*/
+
+    /* Use runtime exceptions to indicate programming errors */
+
+    /* all of the unchecked throwable(s) you implement should subclass RuntimeException */
+
+}
