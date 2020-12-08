2803235bb11c61720d0a3640fd89bb1514bf8b8f
Paul Shirmovski <pshirmovski@gmail.com>
2020-12-08 23:02:31 +0100

    concurrency chapter started

---
+++ src/main/java/concurrency/synchronizemutabledata/Rules.java
@@ -0,0 +1,19 @@
+package concurrency.synchronizemutabledata;
+
+public class Rules {
+
+    /* Synchronization is required for reliable communication between threads
+        as well as for mutual exclusion.
+     */
+
+    /* Do not use Thread.stop.
+     */
+
+    /* Synchronization is not guaranteed to work unless both
+        read and write operations are synchronized.
+     */
+
+    /* confine mutable data to a single thread.
+
+     */
+}
---
+++ src/main/java/concurrency/synchronizemutabledata/StopThreadWithVolatile.java
@@ -0,0 +1,35 @@
+package concurrency.synchronizemutabledata;
+
+import java.util.concurrent.TimeUnit;
+
+public class StopThreadWithVolatile {
+
+    private static volatile boolean stopRequested;
+
+
+
+    public static void main(String[] args)
+
+            throws InterruptedException {
+
+        Thread backgroundThread = new Thread(() -> {
+
+            int i = 0;
+
+            while (!stopRequested)
+
+                i++;
+
+        });
+
+        backgroundThread.start();
+
+
+
+        TimeUnit.SECONDS.sleep(1);
+
+        stopRequested = true;
+
+    }
+
+}
