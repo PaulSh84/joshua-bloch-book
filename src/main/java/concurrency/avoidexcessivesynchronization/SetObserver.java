741ad18dce8636fe37a1df6f95d314be9b17e953
Paul Shirmovski <pshirmovski@gmail.com>
2020-12-11 08:41:46 +0100

    synchronization

---
+++ src/main/java/concurrency/avoidexcessivesynchronization/SetObserver.java
@@ -0,0 +1,9 @@
+package concurrency.avoidexcessivesynchronization;
+
+@FunctionalInterface public interface SetObserver<E> {
+
+    // Invoked when an element is added to the observable set
+
+    void added(ObservableSet<E> set, E element);
+
+}
